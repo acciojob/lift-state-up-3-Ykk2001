6320f25c5a0e5284473537b4e112367784a588ba
Ykk2001 <yogeshkhatake18@gmail.com>
2025-11-20 23:19:47 +0530

    Done

--- src/components/ChildComponent1.js
+++ src/components/ChildComponent1.js
@@ -4,7 +4,7 @@ export default function ChildComponent1({setSelectedOption}) {
   return (
     <div style={{backgroundColor:"brown"}}>
        <h2>Child Component 1</h2>
-       <button onClick={()=>setSelectedOption("Option 1")}>Option 1</button>
+       <button onClick={()=>setSelectedOption(" Option 1")}>Option 1</button>
     </div>
   )
 }
--- src/components/ChildComponent2.js
+++ src/components/ChildComponent2.js
@@ -4,7 +4,7 @@ export default function ChildComponent2({setSelectedOption}) {
   return (
     <div style={{backgroundColor:"yellow"}}>
       <h2>Child Component 2</h2>
-       <button onClick={()=>setSelectedOption("Option 2")}>Option 2</button>
+       <button onClick={()=>setSelectedOption(" Option 2")}>Option 2</button>
     </div>
   )
 }
